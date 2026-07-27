ceff4629e3f0ff2a3429bd9a9a8bbb47f27c190e
cafagnarob <cafagna.rob@gmail.com>
2026-07-27 15:21:14 +0200

    Create LikeRepository.java

---
+++ src/main/java/robertoCafagna/BE_capstone/repositories/LikeRepository.java
@@ -0,0 +1,11 @@
+package robertoCafagna.BE_capstone.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import robertoCafagna.BE_capstone.entities.Like;
+
+import java.util.UUID;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, UUID> {
+}
