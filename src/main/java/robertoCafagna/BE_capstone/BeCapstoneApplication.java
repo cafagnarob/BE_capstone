cfcb5f52420c7fe4555a2e1972e83a1fc6c1d994
cafagnarob <cafagna.rob@gmail.com>
2026-07-27 09:35:41 +0200

    Initial commit

---
+++ src/main/java/robertoCafagna/BE_capstone/BeCapstoneApplication.java
@@ -0,0 +1,13 @@
+package robertoCafagna.BE_capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/robertoCafagna/BE_capstone/BeCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package robertoCafagna.BE_capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
