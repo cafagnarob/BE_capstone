cae61d799a3dc250cbd5cafe77eb8860d1b2aaa9
cafagnarob <cafagna.rob@gmail.com>
2026-07-27 16:04:32 +0200

    Update UserRepository.java

--- src/main/java/robertoCafagna/BE_capstone/repositories/UserRepository.java
+++ src/main/java/robertoCafagna/BE_capstone/repositories/UserRepository.java
@@ -8,4 +8,11 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
 }
