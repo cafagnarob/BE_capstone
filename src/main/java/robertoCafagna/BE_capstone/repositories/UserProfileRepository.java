b653aa47f98a29dc40350424dbe1c29ff125bac9
cafagnarob <cafagna.rob@gmail.com>
2026-07-27 15:20:57 +0200

    Create UserProfileRepository.java

---
+++ src/main/java/robertoCafagna/BE_capstone/repositories/UserProfileRepository.java
@@ -0,0 +1,11 @@
+package robertoCafagna.BE_capstone.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import robertoCafagna.BE_capstone.entities.UserProfile;
+
+import java.util.UUID;
+
+@Repository
+public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
+}
