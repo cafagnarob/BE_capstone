3b147040338e58b4c7aee29c52c2a2c97bafdbb9
cafagnarob <cafagna.rob@gmail.com>
2026-07-27 15:58:57 +0200

    Update UserRepository.java

--- src/main/java/robertoCafagna/BE_capstone/repositories/UserRepository.java
+++ src/main/java/robertoCafagna/BE_capstone/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package robertoCafagna.BE_capstone.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import robertoCafagna.BE_capstone.entities.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
 }
