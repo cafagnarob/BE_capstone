3f0a641add62a088899ac5f1550a3a340d1634a3
cafagnarob <cafagna.rob@gmail.com>
2026-07-27 21:02:56 +0200

    Update MotorcycleModel.java

--- src/main/java/robertoCafagna/BE_capstone/entities/MotorcycleModel.java
+++ src/main/java/robertoCafagna/BE_capstone/entities/MotorcycleModel.java
@@ -3,6 +3,7 @@ package robertoCafagna.BE_capstone.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import robertoCafagna.BE_capstone.enums.MotorcycleCategory;
 
 import java.util.UUID;
@@ -18,29 +19,38 @@ public class MotorcycleModel {
 
 
     @ManyToOne
+    @Setter
     @JoinColumn(name = "brand_id", nullable = false)
     private Brand brand;
 
 
+    @Setter
     @Column(nullable = false)
     private String name;
 
+    @Setter
     @Column(nullable = false)
     private int engineCc;
 
+    @Setter
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private MotorcycleCategory category;
 
+    @Setter
     private int yearStart;
 
+    @Setter
     private Integer yearEnd;
 
+    @Setter
     @Column(nullable = false)
     private Integer horsePower;
 
+    @Setter
     private Integer weightKg;
 
+    @Setter
     private String imageUrl;
 
 
