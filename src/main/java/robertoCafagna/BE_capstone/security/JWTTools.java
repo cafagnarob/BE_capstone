86beba40c815d76a7a01b39a83bfaaf726e85a4b
cafagnarob <cafagna.rob@gmail.com>
2026-07-27 15:45:13 +0200

    Update JWTTools.java

--- src/main/java/robertoCafagna/BE_capstone/security/JWTTools.java
+++ src/main/java/robertoCafagna/BE_capstone/security/JWTTools.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import robertoCafagna.BE_capstone.entities.User;
+import robertoCafagna.BE_capstone.exceptions.UnauthorizedException;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
