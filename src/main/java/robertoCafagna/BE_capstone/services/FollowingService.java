ff90d74c830b85973e21475fa694114bf3453bcf
cafagnarob <cafagna.rob@gmail.com>
2026-07-27 16:28:00 +0200

    Create FollowingService.java

---
+++ src/main/java/robertoCafagna/BE_capstone/services/FollowingService.java
@@ -0,0 +1,16 @@
+package robertoCafagna.BE_capstone.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class FollowingService {
+    //followUser()
+    //unfollowUser()
+    //isFollowing()
+    //getFollowers()
+    //getFollowing()
+}
