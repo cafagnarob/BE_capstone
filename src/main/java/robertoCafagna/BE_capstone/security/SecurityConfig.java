06a0876e336f47c34a108cffeb425e19eaafd30a
cafagnarob <cafagna.rob@gmail.com>
2026-07-27 15:44:30 +0200

    Create SecurityConfig.java

---
+++ src/main/java/robertoCafagna/BE_capstone/security/SecurityConfig.java
@@ -0,0 +1,13 @@
+package robertoCafagna.BE_capstone.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+}
