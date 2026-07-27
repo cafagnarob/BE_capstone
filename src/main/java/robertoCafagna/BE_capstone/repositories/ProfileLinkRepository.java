b072130383ee67af191dc3404f29d23f4704f256
cafagnarob <cafagna.rob@gmail.com>
2026-07-27 15:23:45 +0200

    Update ProfileLinkRepository.java

--- src/main/java/robertoCafagna/BE_capstone/repositories/ProfileLinkRepository.java
+++ src/main/java/robertoCafagna/BE_capstone/repositories/ProfileLinkRepository.java
@@ -4,9 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import robertoCafagna.BE_capstone.entities.ProfileLink;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProfileLinkRepository extends JpaRepository<ProfileLink, UUID> {
-
+    List<ProfileLink> findByProfileId(UUID profileId);
 }
