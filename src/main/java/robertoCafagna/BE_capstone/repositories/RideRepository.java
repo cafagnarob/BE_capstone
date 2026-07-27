4318b3dd83d3f09e0c34a3682ee752ae25d2e0fc
cafagnarob <cafagna.rob@gmail.com>
2026-07-27 18:20:04 +0200

    Update RideRepository.java

--- src/main/java/robertoCafagna/BE_capstone/repositories/RideRepository.java
+++ src/main/java/robertoCafagna/BE_capstone/repositories/RideRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import robertoCafagna.BE_capstone.entities.Ride;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface RideRepository
         extends JpaRepository<Ride, UUID> {
+    List<Ride> findByUserIdOrderByCreatedAtDesc(UUID userId);
+
+    List<Ride> findByVehicleId(UUID vehicleId);
 }
