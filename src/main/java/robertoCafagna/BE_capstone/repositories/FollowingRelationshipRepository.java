f123340a92cfb9575b72a5f8602221c9e7402ee6
cafagnarob <cafagna.rob@gmail.com>
2026-07-27 18:19:40 +0200

    Update FollowingRelationshipRepository.java

--- src/main/java/robertoCafagna/BE_capstone/repositories/FollowingRelationshipRepository.java
+++ src/main/java/robertoCafagna/BE_capstone/repositories/FollowingRelationshipRepository.java
@@ -4,9 +4,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import robertoCafagna.BE_capstone.entities.FollowingRelationship;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface FollowingRelationshipRepository
         extends JpaRepository<FollowingRelationship, UUID> {
+
+    boolean existsByFollowerIdAndFollowedUserId(
+            UUID followerId,
+            UUID followedUserId
+    );
+
+    void deleteByFollowerIdAndFollowedUserId(
+            UUID followerId,
+            UUID followedUserId
+    );
+
+    List<FollowingRelationship> findByFollowerId(UUID userId);
+
+    List<FollowingRelationship> findByFollowedUserId(UUID userId);
+
 }
