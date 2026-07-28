1e505cd8953dfa24e7bdacd847c1471834134463
cafagnarob <cafagna.rob@gmail.com>
2026-07-28 16:02:11 +0200

    Create AdminUserSummaryDTO.java

---
+++ src/main/java/robertoCafagna/BE_capstone/DTO/AdminUserSummaryDTO.java
@@ -0,0 +1,12 @@
+package robertoCafagna.BE_capstone.DTO;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record AdminUserSummaryDTO(UUID id,
+                                  String username,
+                                  String email,
+                                  boolean active,
+                                  LocalDateTime createdAt,
+                                  LocalDateTime lastLogin) {
+}
