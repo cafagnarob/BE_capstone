0ba14468e9354f611dcef7a82b3940d1926ad39e
cafagnarob <cafagna.rob@gmail.com>
2026-07-28 14:36:20 +0200

    Create MyProfileResponseDTO.java

---
+++ src/main/java/robertoCafagna/BE_capstone/DTO/MyProfileResponseDTO.java
@@ -0,0 +1,22 @@
+package robertoCafagna.BE_capstone.DTO;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record MyProfileResponseDTO(
+        UUID id,
+        String username,
+        String name,
+        String surname,
+        String email,
+        String profilePicture,
+        String description,
+        String location,
+        LocalDate birthDate,
+        LocalDateTime createdAt,
+        LocalDateTime lastLogin,
+        boolean active,
+        VehicleSummaryDTO currentVehicle
+) {
+}
