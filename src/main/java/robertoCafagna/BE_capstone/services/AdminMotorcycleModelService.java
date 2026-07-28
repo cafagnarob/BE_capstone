bd7fc8a61e4ba534b6e07ecce5c76759de8fa647
cafagnarob <cafagna.rob@gmail.com>
2026-07-28 21:35:35 +0200

    Update AdminMotorcycleModelService.java

--- src/main/java/robertoCafagna/BE_capstone/services/AdminMotorcycleModelService.java
+++ src/main/java/robertoCafagna/BE_capstone/services/AdminMotorcycleModelService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import robertoCafagna.BE_capstone.DTO.CreateMotorcycleModelRequestDTO;
 import robertoCafagna.BE_capstone.DTO.MotorcycleModelResponseDTO;
 import robertoCafagna.BE_capstone.entities.Brand;
 import robertoCafagna.BE_capstone.entities.MotorcycleModel;
