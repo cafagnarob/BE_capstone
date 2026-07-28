75f4bafc56b8a785ac5404f7e1dc782ab9c1072d
cafagnarob <cafagna.rob@gmail.com>
2026-07-28 16:50:45 +0200

    Create MotorcycleModelResponseDTO.java

---
+++ src/main/java/robertoCafagna/BE_capstone/DTO/MotorcycleModelResponseDTO.java
@@ -0,0 +1,19 @@
+package robertoCafagna.BE_capstone.DTO;
+
+import robertoCafagna.BE_capstone.enums.MotorcycleCategory;
+
+import java.util.UUID;
+
+public record MotorcycleModelResponseDTO(
+        UUID id,
+        BrandResponseDTO brand,
+        String name,
+        int engineCc,
+        MotorcycleCategory category,
+        int yearStart,
+        Integer yearEnd,
+        Integer horsePower,
+        Integer weightKg,
+        String imageUrl
+) {
+}
