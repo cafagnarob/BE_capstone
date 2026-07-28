7cd9599d77723fab04d73979cebfc7ac897b7356
cafagnarob <cafagna.rob@gmail.com>
2026-07-29 01:33:48 +0200

    Update FollowingRelationshipRepository.java

--- src/main/java/robertoCafagna/BE_capstone/repositories/FollowingRelationshipRepository.java
+++ src/main/java/robertoCafagna/BE_capstone/repositories/FollowingRelationshipRepository.java
@@ -1,10 +1,11 @@
 package robertoCafagna.BE_capstone.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import robertoCafagna.BE_capstone.entities.FollowingRelationship;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,22 +14,15 @@ public interface FollowingRelationshipRepository
         extends JpaRepository<FollowingRelationship, UUID> {
 
 
-    void deleteByFollowerIdAndFollowedUserId(
-            UUID followerId,
-            UUID followedUserId
-    );
-
-
     Optional<FollowingRelationship> findByFollowerIdAndFollowedUserId(UUID followerId, UUID followedUserId);
 
     boolean existsByFollowerIdAndFollowedUserId(UUID followerId, UUID followedUserId);
 
-    List<FollowingRelationship> findByFollowerId(UUID followerId);
+    Page<FollowingRelationship> findByFollowerId(UUID followerId, Pageable pageable);
 
-    List<FollowingRelationship> findByFollowedUserId(UUID followedUserId);
+    Page<FollowingRelationship> findByFollowedUserId(UUID followedUserId, Pageable pageable);
 
     long countByFollowerId(UUID followerId);
 
     long countByFollowedUserId(UUID followedUserId);
-
 }
