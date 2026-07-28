7e536cb52340d7878a2eaf6c1f2c1a3e5ea76a04
cafagnarob <cafagna.rob@gmail.com>
2026-07-28 17:09:38 +0200

    Update MotorcycleModelRepository.java

--- src/main/java/robertoCafagna/BE_capstone/repositories/MotorcycleModelRepository.java
+++ src/main/java/robertoCafagna/BE_capstone/repositories/MotorcycleModelRepository.java
@@ -1,19 +1,20 @@
 package robertoCafagna.BE_capstone.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import robertoCafagna.BE_capstone.entities.MotorcycleModel;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface MotorcycleModelRepository
         extends JpaRepository<MotorcycleModel, UUID> {
 
-    List<MotorcycleModel> findByBrandId(UUID brandId);
+    Page<MotorcycleModel> findByBrandId(UUID brandId, Pageable pageable);
 
-    List<MotorcycleModel> findByNameContainingIgnoreCase(String name);
+    Page<MotorcycleModel> findByNameContainingIgnoreCase(String name, Pageable pageable);
 
     boolean existsByBrandIdAndName(
             UUID brandId,
