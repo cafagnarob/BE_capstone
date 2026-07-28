dc3f496dd290c49160bdf454f45376e329433a6e
cafagnarob <cafagna.rob@gmail.com>
2026-07-28 23:39:02 +0200

    Update EventInviteRepository.java

--- src/main/java/robertoCafagna/BE_capstone/repositories/EventInviteRepository.java
+++ src/main/java/robertoCafagna/BE_capstone/repositories/EventInviteRepository.java
@@ -1,5 +1,6 @@
 package robertoCafagna.BE_capstone.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import robertoCafagna.BE_capstone.entities.EventInvite;
 import robertoCafagna.BE_capstone.enums.InviteStatus;
@@ -9,7 +10,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface EventInviteRepository {
+public interface EventInviteRepository extends JpaRepository<EventInvite, UUID> {
     Optional<EventInvite> findByEventIdAndInvitedUserId(UUID eventId, UUID invitedUserId);
 
     List<EventInvite> findByInvitedUserIdAndStatus(UUID invitedUserId, InviteStatus status);
