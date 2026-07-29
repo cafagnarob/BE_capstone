434e0c55aff4cd40da71d22865c0ad1a7f2dfea5
cafagnarob <cafagna.rob@gmail.com>
2026-07-29 10:09:32 +0200

    Create FinishRideRequestDTO.java

---
+++ src/main/java/robertoCafagna/BE_capstone/DTO/FinishRideRequestDTO.java
@@ -0,0 +1,31 @@
+package robertoCafagna.BE_capstone.DTO;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record FinishRideRequestDTO(
+        @NotNull(message = "Specificare l'orario di fine")
+        LocalDateTime endedAt,
+
+        @NotNull(message = "Specificare la distanza percorsa")
+        Double distanceKm,
+
+        Double avgSpeedKmH,
+
+        Double maxSpeedKmH,
+
+        int stopsCount,
+
+        int totalStopDurationSeconds,
+
+        String notes,
+
+        @NotEmpty(message = "Il percorso deve contenere almeno un punto GPS")
+        @Valid
+        List<RidePointRequestDTO> points
+) {
+}
