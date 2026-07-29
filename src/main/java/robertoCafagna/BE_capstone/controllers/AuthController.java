7cfb8434eeae12087f044a4216d5b1d7974ade9c
cafagnarob <cafagna.rob@gmail.com>
2026-07-29 23:57:21 +0200

    Update AuthController.java

--- src/main/java/robertoCafagna/BE_capstone/controllers/AuthController.java
+++ src/main/java/robertoCafagna/BE_capstone/controllers/AuthController.java
@@ -2,10 +2,7 @@ package robertoCafagna.BE_capstone.controllers;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import robertoCafagna.BE_capstone.DTO.*;
 import robertoCafagna.BE_capstone.services.AuthService;
 import robertoCafagna.BE_capstone.services.PasswordResetService;
@@ -42,6 +39,17 @@ public class AuthController {
     public ResponseEntity<String> resetPassword(@RequestBody @Valid ResetPasswordRequestDTO body) {
         passwordResetService.resetPassword(body.token(), body.newPassword());
         return ResponseEntity.ok("Password reimpostata con successo");
+    }
+
+    @GetMapping("/verify-email")
+    public ResponseEntity<String> verifyEmail(@RequestParam String token) {
+        return ResponseEntity.ok(authService.verifyEmail(token));
+    }
+
+    @PostMapping("/resend-verification")
+    public ResponseEntity<String> resendVerification(@RequestBody @Valid ForgotPasswordRequestDTO body) {
+        authService.resendVerificationEmail(body.email());
+        return ResponseEntity.ok("Se l'indirizzo email è registrato e non ancora verificato, riceverai un nuovo link");
     }
 
 }
