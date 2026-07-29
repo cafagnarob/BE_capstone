e5a10280ec96b2f8b0e2434704a0fe0cb0c6c45e
cafagnarob <cafagna.rob@gmail.com>
2026-07-29 11:29:25 +0200

    Update LikeRepository.java

--- src/main/java/robertoCafagna/BE_capstone/repositories/LikeRepository.java
+++ src/main/java/robertoCafagna/BE_capstone/repositories/LikeRepository.java
@@ -4,19 +4,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import robertoCafagna.BE_capstone.entities.Like;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, UUID> {
-    boolean existsByUserIdAndPostId(
-            UUID userId,
-            UUID postId
-    );
+
 
     void deleteByUserIdAndPostId(
             UUID userId,
             UUID postId
     );
+
+
+    Optional<Like> findByUserIdAndPostId(UUID userId, UUID postId);
+
+    boolean existsByUserIdAndPostId(UUID userId, UUID postId);
 
     long countByPostId(UUID postId);
 }
