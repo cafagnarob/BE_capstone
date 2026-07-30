d496dd63a7dab14d0cb2243b3a2294aab35a73c1
cafagnarob <cafagna.rob@gmail.com>
2026-07-30 10:50:41 +0200

    Update RouteRepository.java

--- src/main/java/robertoCafagna/BE_capstone/repositories/RIDE/RouteRepository.java
+++ src/main/java/robertoCafagna/BE_capstone/repositories/RIDE/RouteRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import robertoCafagna.BE_capstone.entities.Route;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface RouteRepository extends JpaRepository<Route, UUID> {
+
+    long countByCreatorIdAndCreatedAtAfter(UUID creatorId, LocalDateTime after);
 
     Page<Route> findByCreatorIdOrderByCreatedAtDesc(UUID creatorId, Pageable pageable);
 }
