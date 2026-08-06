7c6eebc3ec2d42c134352c18b09e453e7c91b066
cafagnarob <cafagna.rob@gmail.com>
2026-08-07 00:25:58 +0200

    Update RegenerateAccessCodeRequestDTO.java

--- src/main/java/robertoCafagna/BE_capstone/DTO/EVENT/RegenerateAccessCodeRequestDTO.java
+++ src/main/java/robertoCafagna/BE_capstone/DTO/EVENT/RegenerateAccessCodeRequestDTO.java
@@ -3,6 +3,8 @@ package robertoCafagna.BE_capstone.DTO.EVENT;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegenerateAccessCodeRequestDTO(
+        @NotBlank(message = "Password mancante")
+        String currentPassword,
         @NotBlank(message = "Inserire il nuovo codice di accesso")
         String newAccessCode
 ) {
