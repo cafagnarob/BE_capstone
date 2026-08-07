a934ae985c37636d4a11aa6740d41dcb07d9e3bd
cafagnarob <cafagna.rob@gmail.com>
2026-08-07 12:41:31 +0200

    Update UserRepository.java

--- src/main/java/robertoCafagna/BE_capstone/repositories/USER/UserRepository.java
+++ src/main/java/robertoCafagna/BE_capstone/repositories/USER/UserRepository.java
@@ -1,6 +1,10 @@
 package robertoCafagna.BE_capstone.repositories.USER;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import robertoCafagna.BE_capstone.entities.User;
 
 import java.util.Optional;
@@ -18,4 +22,12 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByResetPasswordToken(String resetPasswordToken);
 
     Optional<User> findByEmailVerificationToken(String token);
+
+    @Query("SELECT u FROM User u WHERE u.active = true AND (" +
+            "LOWER(u.username) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+            "LOWER(u.name) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+            "LOWER(u.surname) LIKE LOWER(CONCAT('%', :query, '%')))")
+    Page<User> searchActive(@Param("query") String query, Pageable pageable);
+
+
 }
